fa2c76abff2efc54dbec59f5944719496034180d
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2024-08-25 21:51:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//Campos para almacenar las llaves de encriptacion
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+
+
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
+function btnDesencriptar() {
+    const textoEncriptado = desEncriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+
+function desEncriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+
+document.getElementById('inputText').addEventListener('input', function (event) {
+    const warningMessage = document.getElementById('warningMessage');
+    const inputText = event.target.value;
+
+    const invalidChars = /[A-ZÁÉÍÓÚÑ0-9!@#$%^&*(),.?":{}|<>[\]¨{}¿]/;
+
+    if (invalidChars.test(inputText)) {
+        warningMessage.style.color = 'red';
+        event.target.value = inputText.toLowerCase().replace(/[^a-z\s]/g, '');
+    } else {
+        warningMessage.style.color = 'black';
+    }
+});
